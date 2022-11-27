c9670f52e9198fb4574b9d034672a835302c20f6
Valeria <v.rumyantceva@gmail.com>
2022-11-27 21:46:15 +0300

    task complete

--- Task/Program.cs
+++ Task/Program.cs
@@ -10,7 +10,6 @@ int size = Convert.ToInt32(Console.ReadLine());
 string[] FillArraySrt(int size)
 {
     string[] arr = new string[size];
-
     for (int i = 0; i < size; i++)
     {
         Console.WriteLine("Введите элемент массива: ");
@@ -21,24 +20,36 @@ string[] FillArraySrt(int size)
 
 string[] LimitedSizeArray(string[] array1)
 {
-    string[] arr2 = new string[array1.Length];
-}
+    int size2 = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array1[i].Length <= 3) size2 += 1;
+    }
 
+    string[] arr2 = new string[size2];
+    int count = 0;
+    for (int i = 0; i <= size2; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            arr2[count] = array1[i];
+            count++;
+        }
+    }
+    return arr2;
+}
 
 void PrintArray(string[] array)
 {
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if(i<array.Length - 1) Console.Write($"{array[i]}, ");
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
         else Console.Write($"{array[i]}");
     }
     Console.WriteLine("]");
 }
 
 string[] array1 = FillArraySrt(size);
-PrintArray(array1);
-
-
-// SecondArrayWithIF(array1, array2);
-// PrintArray(arr);
+string[] array2 = LimitedSizeArray(array1);
+PrintArray(array2);
